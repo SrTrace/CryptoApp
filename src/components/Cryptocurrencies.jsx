c75b5577b6cc37a007c43742989ea2379a33b813
Sergey <levashov04sergey@gmail.com>
2021-09-25 16:36:05 +0300

    create News component

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,7 +6,6 @@ import {Card, Row, Col, Input} from 'antd';
 import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const Cryptocurrencies = ({simplified}) => {
-    console.log(simplified);
     const count = simplified ? 10 : 100;
     const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState([]);
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -31,7 +31,7 @@ const Homepage = () => {
             <Cryptocurrencies simplified/>
             <div className="home-heading-container">
                 <Title level={2} className="home-title">Latest Crypto News</Title>
-                <Title level={3} className="show-more"><Link to="/news">News</Link></Title>
+                <Title level={3} className="show-more"><Link to="/news">Show More</Link></Title>
             </div>
             <News simplified/>
         </>
